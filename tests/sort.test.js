9ff4a573228bfad0c1aaa02a3d1cd6e1ad82e321
Rodolphe-74 <rodolphe.gacougnolle@le-campus-numerique.fr>
2022-01-27 10:42:38 +0100

    Amélioration du test de tri

--- tests/sort.test.js
+++ tests/sort.test.js
@@ -11,13 +11,17 @@ describe("Checkout process", () => {
         await page.waitForSelector('#header_container > div.header_secondary_container > div.right_component > span > select > option:nth-child(3)');
         await page.select('#header_container > div.header_secondary_container > div.right_component > span > select', 'lohi');
         await page.screenshot({path: './tests/img/tri.png'});
-        let array = [];
-        for(let i=1;i<7;i++){
-            const text = await page.$eval("#inventory_container > div > div:nth-child( "+ i +") > div.inventory_item_description > div.pricebar > div", e => e.innerHTML);
-            let newtext = text.substr(1)
-            let number =  parseFloat(newtext)
-            array.push(number);
-        }
+        const array = await page.$$eval(".inventory_item_price", e => e.map((t)=> parseFloat(t.innerHTML.substr(1))));
+        console.log(array);
+
+        //***************** Code avec boucle for avant refactoring *************************
+        // for(let i=1;i<7;i++){
+        //     const text = await page.$eval("#inventory_container > div > div:nth-child( "+ i +") > div.inventory_item_description > div.pricebar > div", e => e.innerHTML);
+        //     let newtext = text.substr(1)
+        //     let number =  parseFloat(newtext)
+        //     array.push(number);
+        // }
+
         let isSorted = true
         for(let i=0;i<array.length-1;i++){
             if(array[i]>array[i+1]){
